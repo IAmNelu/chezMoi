54cb15c6aaeb0ce8fbb0a6bc42099553f73a0339
Valentin Nelu <ifrimvale@gmail.com>
2020-12-24 15:57:03 +0100

    Host screen skeleton

--- scripts/valentin.js
+++ scripts/valentin.js
@@ -4,16 +4,41 @@ window.onload = function () {
 
 };
 
+window.onresize = function () {
+    adjust_header();
+    adjust_profile();
+}
 
 function adjust_profile() {
     let prof_av = $('.avatar_profile').first();
     let w = prof_av.width();
     prof_av.height(w);
-    prof_av.css('margin-top', '-60pt');
+    if (window.innerHeight > window.innerWidth) {
+        //portrait
+        prof_av.css('margin-top', '-6em');
+    } else {
+        prof_av.css('margin-top', '-16em');
+    }
+
 
 }
 
 function adjust_header() {
     let prof_av = $('.main_location_pic').first();
-    prof_av.css('height', '150pt');
+    if (window.innerHeight > window.innerWidth) {
+        //portrait
+        prof_av.css('height', '12em');
+    }
+    else {//landscape
+        prof_av.css('height', '32em');
+    }
+
+}
+
+function hide_small_description() {
+    $('#small_description').hide();
+}
+
+function show_small_description() {
+    $('#small_description').show();
 }
