aa3351d6fb1e69b4bd766cc0cf458f3f049da25b
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2020-12-24 20:01:26 +0100

    added: user home

---
+++ scripts/vincenzo.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function showMap(){    
+    var map = L.map('mapid').setView([51.505, -0.11], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var cooking_mama = L.marker([51.5, -0.09]).addTo(map);
+    
+    var you = L.marker([51.508, -0.11]).addTo(map);
+    
+    cooking_mama.bindPopup('<img src="images/profile_cooking_mama.png" class="w-100"><br><br><b>Cooking mama</b>')
+        .openPopup();
+    
+    you.bindPopup('<i class="fa fa-user-circle-o btn_ico" aria-hidden="true"></i><br><br><b>You are here</b>')
+        .openPopup();
+}
+
+showMap()
